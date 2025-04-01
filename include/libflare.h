5f9e9ad995529f58af4be12713f702db98f91a33
Maxime Djmb <maxime.djomby@gmail.com>
2025-04-02 01:31:59 +0200

    VK Validation Layers

--- include/libflare.h
+++ include/libflare.h
@@ -1,26 +1,81 @@
 #ifndef FLARE_LIBRARY_H
 #define FLARE_LIBRARY_H
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vector>
+
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
+
+VkResult CreateDebugUtilsMessengerEXT(
+    VkInstance instance,
+    const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkDebugUtilsMessengerEXT* pDebugMessenger
+    );
+
+void DestroyDebugUtilsMessengerEXT(
+    VkInstance instance,
+    VkDebugUtilsMessengerEXT debugMessenger,
+    const VkAllocationCallbacks* pAllocator
+    );
+
 class HelloTriangleApplication {
-public:
-    void run() {
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
 
-private:
-    void initVulkan() {
+    public:
+    void run();
 
-    }
+    private:
+    GLFWwindow* window = nullptr;
 
-    void mainLoop() {
+    VkInstance instance = nullptr;
+    VkDebugUtilsMessengerEXT debugMessenger = nullptr;
 
-    }
+    bool exitLoop = false;
 
-    void cleanup() {
+    void initWindow();
 
-    }
+    void initVulkan();
+
+    void mainLoop();
+
+    void requestExit();
+
+    void listenForExit();
+
+    void cleanup();
+
+    void createInstance();
+
+    static void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+
+    void setupDebugMessenger();
+
+    static std::vector<const char*> getRequiredExtensions();
+
+    static bool checkExtensionSupport(const std::vector<const char*>& requiredExtensions);
+
+    static bool checkValidationLayerSupport();
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData
+        );
+
 };
 
 #endif //FLARE_LIBRARY_H
